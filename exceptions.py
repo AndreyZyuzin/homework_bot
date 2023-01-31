b9f4e7c4ebdcfefb1764dd3c9329dd9ac4f8748f
Андрей Зюзин <andrey.bp.zyuzin@yandex.ru>
2023-01-31 08:29:58 +0300

    Done app satisfying pytest

--- exceptions.py
+++ exceptions.py
@@ -1,13 +1,47 @@
 class TokenError(Exception):
     """Класс исключений из-за неверного токена."""
 
-    def __init__(self, training_type: str) -> None:
+    def __init__(self):
         """Начальная установка исключения."""
-        super().__init__(f'{type(self).__name__}: '
-                         f'Unknown type - "{training_type}"')
-        self.training_type = training_type
+        super().__init__('Error token')
 
     def __str__(self):
         """Сообщение исключения."""
-        return (f'"{self.training_type}" - '
-                'не определён тип тренировки.')
+        return ('Не подходит токен')
+
+
+class StatusCodeNot200RequestsError(Exception):
+    """Исключение при неудачи при связи с эндпоинтом."""
+
+    def __init__(self, status_code=None, endpoint=None):
+        """Инициализация исключение."""
+        message = f'{type(self).__name__}: '
+        if endpoint:
+            message += f'Endpoint - "{endpoint}".'
+        if status_code:
+            message = f' status_code - {status_code}.'
+        super().__init__(message)
+        self.status_code = status_code
+        self.endpoint = endpoint
+
+    def __str__(self):
+        """Сообщение исключения."""
+        message = ''
+        if self.endpoint:
+            message += f'При соединении с {self.endpoint} '
+        message = 'произошла ошибка.'
+        if self.status_code:
+            message += f' status_code - {self.status_code}.'
+        return (message)
+
+
+class APIResponseNotMatchError(Exception):
+    """Класс исключений API не соответствует с документации."""
+
+    def __init__(self):
+        """Начальная установка исключения."""
+        super().__init__('Strange API response')
+
+    def __str__(self):
+        """Сообщение исключения."""
+        return ('Ответ API не соответствует с документации')
--- homework.py
+++ homework.py
@@ -1,12 +1,14 @@
 import os
 from dotenv import load_dotenv
 import requests
-from datetime import datetime, timedelta, timezone
+from http import HTTPStatus
 import time
 import telegram
-import app_logger
+import logging
+import exceptions
 
-logger = app_logger.get_logger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s, %(levelname)s, %(message)s')
 load_dotenv()
 
 
@@ -14,7 +16,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_PERIOD = 6
+RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -25,10 +27,21 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-VIEWABLE_DAYS_TO_REQUEST_PROJECTS = 7 * 14
 
 def check_tokens():
     """Проверка переменных окружения."""
+    # if 'PRACTICUM_TOKEN' not in globals():
+    #     raise exceptions.TokenError
+    # if 'TELEGRAM_TOKEN' not in globals():
+    #     raise exceptions.TokenError
+    # if 'TELEGRAM_CHAT_ID' not in globals():
+    #     raise exceptions.TokenError
+    if PRACTICUM_TOKEN is None:
+        raise exceptions.TokenError
+    if TELEGRAM_TOKEN is None:
+        raise exceptions.TokenError
+    if TELEGRAM_CHAT_ID is None:
+        raise exceptions.TokenError
     url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates'
     response = requests.get(url).json()
     if not response.get('ok'):
@@ -41,83 +54,102 @@ def check_tokens():
 
 def send_message(bot, message):
     """Отправка сообщения в Telegram-чат."""
-    bot.send_message(text=message, chat_id=TELEGRAM_CHAT_ID)
+    try:
+        bot.send_message(text=message, chat_id=TELEGRAM_CHAT_ID)
+    except Exception as error:
+        logging.error(error, exc_info=False)
+        raise telegram.error.TelegramError
+    logging.debug(f'Отправлено сообщение в чат телеграмма: "{message}"')
 
 
 def get_api_answer(timestamp):
     """Получить запрос к ендпоинту."""
-    return requests.get(
-        ENDPOINT,
-        headers=HEADERS,
-        params={'from_date': timestamp}
-    ).json()
+    try:
+        response = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params={'from_date': timestamp}
+        )
+    except requests.RequestException as error:
+        raise Exception('adf')
+    if response.status_code != HTTPStatus.OK:
+        raise exceptions.StatusCodeNot200RequestsError(
+            response.status_code, ENDPOINT)
+
+    return response.json()
 
 
 def check_response(response):
     """Проверка ответа API на соответствие документации."""
+    if not isinstance(response, dict):
+        raise TypeError
+    if 'homeworks' not in response:
+        raise exceptions.APIResponseNotMatchError
+    homeworks = response.get('homeworks')
+    if not isinstance(homeworks, list):
+        raise TypeError
+    homework = homeworks[0] if len(homeworks) else None
+    if homework and 'homework_name' not in homework:
+        raise exceptions.APIResponseNotMatchError
+    # if 'status' not in homework:
+    #     raise exceptions.APIResponseNotMatchError
     return True
 
 
 def parse_status(homework):
     """Извлечения информации о домашней работе."""
-    if homework:
-        return (
-            f'Изменился статус проверки работы "{homework["lesson_name32"]}".'
-            f'{HOMEWORK_VERDICTS[homework["status"]]}'
-        )
-    return (f'За последние {VIEWABLE_DAYS_TO_REQUEST_PROJECTS} дней '
-            'домашнее задания не отправлялось')
-
-
-def get_last_homework(homeworks):
-    """Получить последнюю работу."""
-    if len(homeworks) == 0:
-        return {}
-    # return sorted(homeworks, key=lambda hw: hw['id'], reverse=True)[0]
-    return sorted(
-        homeworks,
-        key=lambda hw: datetime.strptime(hw['date_updated'],
-                                         '%Y-%m-%dT%H:%M:%S%z'),
-        reverse=True
-    )[0]
+    if not isinstance(homework, dict):
+        return
+    if 'homework_name' not in homework:
+        raise Exception('Понятный текст')
+    if 'status' not in homework:
+        raise Exception('Понятный текст')
+    if homework["status"] not in HOMEWORK_VERDICTS:
+        raise exceptions.TokenError
+    return (
+        f'Изменился статус проверки работы "{homework["homework_name"]}".'
+        f'{HOMEWORK_VERDICTS[homework["status"]]}'
+    )
 
 
 def main():
     """Основная логика работы бота."""
-    logger.info('Старт программы')
-    check_tokens()
+    logging.info('Старт программы')
+    try:
+        check_tokens()
+    except Exception as error:
+        logging.critical(error, exc_info=False)
+        raise SystemExit
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    last_updated_homework = None
+    last_status = None
 
     while True:
         try:
-            from_date = (datetime.now(timezone.utc)
-                         - timedelta(days=VIEWABLE_DAYS_TO_REQUEST_PROJECTS))
-            unix_timestamp = int(from_date.timestamp())
-
-            response = get_api_answer(unix_timestamp)
-
-            if not check_response(response):
-                pass
-
-            homework = get_last_homework(response.get('homeworks'))
-            if not last_updated_homework == homework:
-                last_updated_homework = homework.copy()
-                status = parse_status(homework)
+            response = get_api_answer(0)
+            check_response(response)
+            homework = response.get('homeworks')
+            if isinstance(homework, list):
+                if len(homework):
+                    homework = homework[0]
+            status = parse_status(homework)
+            if not last_status == status:
+                last_status = status
                 send_message(bot, status)
-                logger.debug(status)
+                logging.debug(status)
             else:
-                logger.debug('Нет изменения статуса')
+                logging.debug('Нет изменения статуса')
             time.sleep(RETRY_PERIOD)
+        except requests.RequestException as error:
+            raise
         except Exception as error:
             error_text = f'{type(error).__name__}: {error}'
             send_message(bot, f'Сбой в работе программы: {error_text}')
-            logger.exception(f'{error_text}', exc_info=True)
+            logging.exception(f'{error_text}', exc_info=True)
             raise
 
 
-    logger.info('Завершение работы программы')
+    logging.info('Завершение работы программы')
 
 
 if __name__ == '__main__':
